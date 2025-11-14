a9b227ff5db01dbdff69e07b05a5a6eae9fdb468
gegen <paulsimon.ps03@gmail.com>
2025-11-14 09:58:26 +0100

    Aufgabe e, f

--- praktikum2/defines.h
+++ praktikum2/defines.h
@@ -8,14 +8,19 @@
   #define DEBUG_PRINT(x)
   #define DEBUG_PRINTLN(x)
 #endif
-#if defined(ARDUINO_ARCH_SAM) 
-  #define ADC_RESOLUTION_BITS 12 
-  #define SETRES(x) analogReadResolution(x) 
+#if defined(ARDUINO_ARCH_SAM)
+#define MAX_VOLTAGE 3300
+#define WireMaster Wire1
+#define SETRES(x) analogReadResolution(x)
+// red board
 #elif defined(ARDUINO_ARCH_AVR)
-  #define ADC_RESOLUTION_BITS 10 
-  #define SETRES(x) while(false){} 
+#define ADC_RESOLUTION 10
+#define MAX_VOLTAGE 5000
+#define WireMaster Wire
+#define SETRES(x) while(false){}
+// untested
 #else
   #error "Wrong Hardware"
 #endif
-const unsigned long ADCMax = (1UL << ADC_RESOLUTION_BITS) - 1;
+const unsigned long ADCMax = (1 << ADC_RESOLUTION) - 1;
 #endif
--- praktikum2/praktikum2.ino
+++ praktikum2/praktikum2.ino
@@ -1,31 +1,44 @@
 #include <Arduino.h>
+
+#define DEBUG
 #include "defines.h"
-#include <LiquidCrystal_I2C.h>
 
 #include <Servo.h>
 #include <math.h> 
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-const int POTI_PIN = A1;
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+
 Servo meinServo;
-const int LED_PIN = LED_BUILTIN; 
-const long LED_INTERVAL = 3000;    
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+
+const int POTI_PIN = A1;
+
+const int TASTER_PIN = 3;
+volatile bool tasterGedrueckt = false;
+
+const int LED_PIN = LED_BUILTIN;
+
+//Intervall
+bool turboMode = false;
+unsigned long turboStartMillis = 0;
+unsigned long loopStartMillis = 0;
 unsigned long previousLedMillis = 0;
 
-const int TASTER_PIN = 2; 
-volatile bool tasterGedrueckt = false; 
+const long NORMAL_INTERVAL = 3000;
+const long TURBO_INTERVAL  = 500;    // 0,5 Sekunden
+const long TURBO_DURATION  = 60000;  // 1 Minute
 
-const long LOOP_CHECK_INTERVAL = 500; 
-unsigned long previousLoopMillis = 0;
-unsigned long loopStartMillis = 0;
-const long ONE_MINUTE = 60000; 
-
+//Temp
 const int pinTempSensor = A0;
-const unsigned int B = 4275;      // b wert anpassen für ntc
+const unsigned int B = 4275;  // b wert anpassen für ntc
 const float T0_KELVIN = 298.15; 
 float currentTempC = 0.0;
-
-DEBUG
 
 void handleTasterPress() {
   tasterGedrueckt = true;
@@ -34,58 +47,73 @@ void setup() {
   Serial.begin(9600); 
   DEBUG_PRINTLN("Praktikum 2");
 
-  SETRES(ADC_RESOLUTION_BITS);
+  SETRES(12);
 
   pinMode(LED_PIN, OUTPUT);
 
   pinMode(TASTER_PIN, INPUT_PULLUP); 
   attachInterrupt(digitalPinToInterrupt(TASTER_PIN), handleTasterPress, FALLING); 
-  meinServo.attach(4); 
+  meinServo.attach(7);
 
-  lcd.init();
-  lcd.backlight();
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2, LCD_5x8DOTS, WireMaster);
+
+  lcd.setRGB(colorR, colorG, colorB);
+
+  lcd.print("hello, world!");
 }
 void loop() {
   unsigned long currentMillis = millis();
+  unsigned long interval = turboMode ? TURBO_INTERVAL : NORMAL_INTERVAL;
   
-  DEBUG_PRINT("Loopstart @ ");
-  DEBUG_PRINTLN(currentMillis);
+  // DEBUG_PRINT("Loopstart @ ");
+  // DEBUG_PRINTLN(currentMillis);
   
   if (loopStartMillis == 0) {
       loopStartMillis = currentMillis;
   }
 
-  if (currentMillis - loopStartMillis < ONE_MINUTE) { 
-      if (currentMillis - previousLoopMillis >= LOOP_CHECK_INTERVAL) {
-          previousLoopMillis = currentMillis;
-          
-          DEBUG_PRINTLN("0.5s fertgi");
-      }
+
+  // 3 Sekunden Durchlauf
+  if (currentMillis - previousLedMillis >= interval) {
+    previousLedMillis = currentMillis;
+    logic();
   }
 
-  if (currentMillis - previousLedMillis >= LED_INTERVAL) {
-    previousLedMillis = currentMillis;
-    int ledState = digitalRead(LED_PIN);
-    digitalWrite(LED_PIN, !ledState);
+  if (turboMode && (millis() - turboStartMillis >= TURBO_DURATION)) {
+      turboMode = false;
+      DEBUG_PRINTLN("Turbo-Modus beendet");
   }
 
   if (tasterGedrueckt) {
     DEBUG_PRINTLN("Taster per Interrupt erkannt");
-    tasterGedrueckt = false; 
+    tasterGedrueckt = false;
+
+    turboMode = true;
+    turboStartMillis = millis();
   }
 
-  DEBUG_PRINT("Loopende @ ");
-  DEBUG_PRINTLN(millis());
+  // DEBUG_PRINT("Loopende @ ");
+  // DEBUG_PRINTLN(millis());
+}
 
+void logic() {
+  // LED Updaten
+  int ledState = digitalRead(LED_PIN);
+  digitalWrite(LED_PIN, !ledState);
+  
+  // Servo
   int potiValue = analogRead(POTI_PIN);
-  int servoAngle = map(potiValue, 0, ADCMax, 5, 175);
+  int servoAngle = map(potiValue, 0, ADCMax, 5, 160);
 
   meinServo.write(servoAngle);
 
   DEBUG_PRINT("Servo Winkel: ");
-  DEBUG_PRINTLN(servoAngle);
-  DEBUG_PRINTLN(" Grad");
+  DEBUG_PRINT(servoAngle);
+  DEBUG_PRINTLN("°");
 
+  // Temp
   unsigned int tempRawValue = analogRead(pinTempSensor);
   float resistance_ratio = ((float)ADCMax / tempRawValue) - 1.0;
   float currentTempK = 1.0 / ((log(resistance_ratio) / B) + (1.0 / T0_KELVIN));
@@ -95,16 +123,19 @@ void loop() {
   DEBUG_PRINT(currentTempC);
   DEBUG_PRINTLN(" °C");
 
-  //2f
+  updateLCD(currentTempC, servoAngle);
+}
 
+void updateLCD(float currentTempC, int servoAngle) {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("T: ");
   lcd.print(currentTempC, 1);
   lcd.write(223); // grad code
-  lcd.print("C   ");
+  lcd.print("C");
 
   lcd.setCursor(0, 1);
   lcd.print("S: ");
   lcd.print(servoAngle);
-  lcd.print("  deg");
+  lcd.write(223); // grad code
 }
